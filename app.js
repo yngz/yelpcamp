3d33f91da077c400d79f3927c086cbaf038435d7
Yang Zou <yang0201@outlook.com>
2022-03-01 16:36:51 +0000

    Add form to edit campground

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
 const app = express();
@@ -16,6 +17,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 // middlewares
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 // routes
 app.get('/', (req, res) => {
@@ -37,9 +39,20 @@ app.get('/campgrounds/:id', async (req, res) => {
   res.render('campgrounds/show', { campground });
 });
 
+app.get('/campgrounds/:id/edit', async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render('campgrounds/edit', { campground });
+});
+
 app.post('/campgrounds', async (req, res) => {
   const campground = new Campground(req.body.campground);
   await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.put('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
   res.redirect(`/campgrounds/${campground._id}`);
 });
 
