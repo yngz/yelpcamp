ff1bac057a9ebfe76d2cb0c5a3830ea74ebc3021
Yang Zou <yang0201@outlook.com>
2022-03-01 15:04:32 +0000

    Add form for new campground

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ mongoose.connect('mongodb://localhost:27017/yelpcamp')
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// middlewares
+app.use(express.urlencoded({ extended: true }));
+
+// routes
 app.get('/', (req, res) => {
   res.render('home');
 });
@@ -23,9 +27,20 @@ app.get('/campgrounds', async (req, res) => {
   res.render('campgrounds/index', { campgrounds });
 });
 
+// needs to be defined before the :id route
+app.get('/campgrounds/new', (req, res) => {
+  res.render('campgrounds/new');
+});
+
 app.get('/campgrounds/:id', async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   res.render('campgrounds/show', { campground });
+});
+
+app.post('/campgrounds', async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.listen(3000, () => {
