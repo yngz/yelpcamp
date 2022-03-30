38c065dfec65cbdd6c82d924d688b1d2cf0ffa0f
Yang Zou <yang0201@outlook.com>
2022-03-30 22:28:36 +0100

    Create and render an error template

--- app.js
+++ app.js
@@ -74,9 +74,11 @@ app.all('*', (req, res, next) => {
 });
 
 // Custom error handler
+// eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
-  const { statusCode = 500, message = 'Something went wrong' } = err;
-  res.status(statusCode).send(message);
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = 'Oh no, something went wrong!';
+  res.status(statusCode).render('error', { err });
 });
 
 app.listen(3000, () => {
