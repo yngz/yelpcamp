165cbf26ff2814d05979df5048f9ef0c30cc6de6
Yang Zou <yang0201@outlook.com>
2022-04-27 20:41:16 +0100

    Add dir for static assets

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.set('views', path.join(__dirname, 'views'));
 // middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // routes
 app.get('/', (req, res) => {
