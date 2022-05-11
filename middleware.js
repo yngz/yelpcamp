71dd022a7e208daf4090ed3558546a0e6aed1d25
Yang Zou <yang0201@outlook.com>
2022-05-11 09:47:45 +0100

    After login redirect to the original requested url

--- middleware.js
+++ middleware.js
@@ -1,5 +1,7 @@
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
+    // store the url of the unsuccessful request
+    req.session.returnTo = req.originalUrl;
     req.flash('error', 'You must be logged in first!');
     res.redirect('/login');
     return;
