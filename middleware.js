2a345173a6098ae9c4540fee20b80c988af95aaf
Yang Zou <yang0201@outlook.com>
2022-05-21 20:27:59 +0100

    Protect review deletion with isReviewAuthor middleware

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 const Campground = require('./models/campground');
+const Review = require('./models/review');
 const { campgroundSchema, reviewSchema } = require('./schemas');
 const ExpressError = require('./utils/ExpressError');
 
@@ -17,6 +18,17 @@ module.exports.isAuthor = async (req, res, next) => {
   const { id } = req.params;
   const campground = await Campground.findById(id);
   if (!campground.author.equals(req.user._id)) {
+    req.flash('error', 'You do not have permission to do that!');
+    res.redirect(`/campgrounds/${id}`);
+    return;
+  }
+  next();
+};
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+  const { id, reviewId } = req.params;
+  const review = await Review.findById(reviewId);
+  if (!review.author.equals(req.user._id)) {
     req.flash('error', 'You do not have permission to do that!');
     res.redirect(`/campgrounds/${id}`);
     return;
