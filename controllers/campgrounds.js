24e47980a5722d1f1aa42375f98d7f0cc3f46056
Yang Zou <yang0201@outlook.com>
2022-06-06 23:01:16 +0100

    Support deletion of images via edit form

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -49,6 +49,12 @@ module.exports.updateCampground = async (req, res) => {
   const imgs = req.files.map((f) => ({ url: f.path, filename: f.filename }));
   campground.images.push(...imgs);
   await campground.save();
+  if (req.body.deleteImages) {
+    await campground.updateOne({
+      $pull: { images: { filename: { $in: req.body.deleteImages } } },
+    });
+    console.log(campground);
+  }
   req.flash('success', 'Successfully updated campground!');
   res.redirect(`/campgrounds/${campground._id}`);
 };
