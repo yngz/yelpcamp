1b2d01255e6015efbdeac8cab99782ff8eae0c8b
Yang Zou <yang0201@outlook.com>
2022-06-12 18:59:16 +0100

    Add a popup to the pin

--- public/scripts/showPageMap.js
+++ public/scripts/showPageMap.js
@@ -1,11 +1,19 @@
 mapboxgl.accessToken = mapboxToken;
+campground_obj = JSON.parse(campground);
+
 const map = new mapboxgl.Map({
   container: 'map', // container ID
   style: 'mapbox://styles/mapbox/streets-v11', // style URL
-  center: JSON.parse(campground).geometry.coordinates, // starting position [lng, lat]
+  center: campground_obj.geometry.coordinates, // starting position [lng, lat]
   zoom: 10, // starting zoom
 });
 
 new mapboxgl.Marker()
-  .setLngLat(JSON.parse(campground).geometry.coordinates)
+  .setLngLat(campground_obj.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 })
+      .setHTML(
+        `<h3>${campground_obj.title}</h3><p>${campground_obj.location}</p>`,
+      ),
+  )
   .addTo(map);
