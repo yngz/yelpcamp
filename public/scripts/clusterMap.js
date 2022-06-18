8d5fa382b4fa5e7f8d43385f6fa0ef5e95ef0940
Yang Zou <yang0201@outlook.com>
2022-06-18 17:16:06 +0100

    Change map source name

--- public/scripts/clusterMap.js
+++ public/scripts/clusterMap.js
@@ -2,7 +2,7 @@
 mapboxgl.accessToken = mapboxToken
 const map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/mapbox/dark-v10',
+  style: 'mapbox://styles/mapbox/streets-v11',
   center: [-103.5917, 40.6699],
   zoom: 3,
 });
@@ -11,7 +11,7 @@ map.on('load', () => {
 // Add a new source from our GeoJSON data and
 // set the 'cluster' option to true. GL-JS will
 // add the point_count property to your source data.
-  map.addSource('earthquakes', {
+  map.addSource('campgrounds', {
     type: 'geojson',
     // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
     // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
@@ -25,7 +25,7 @@ map.on('load', () => {
   map.addLayer({
     id: 'clusters',
     type: 'circle',
-    source: 'earthquakes',
+    source: 'campgrounds',
     filter: ['has', 'point_count'],
     paint: {
       // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
@@ -57,7 +57,7 @@ map.on('load', () => {
   map.addLayer({
     id: 'cluster-count',
     type: 'symbol',
-    source: 'earthquakes',
+    source: 'campgrounds',
     filter: ['has', 'point_count'],
     layout: {
       'text-field': '{point_count_abbreviated}',
@@ -69,7 +69,7 @@ map.on('load', () => {
   map.addLayer({
     id: 'unclustered-point',
     type: 'circle',
-    source: 'earthquakes',
+    source: 'campgrounds',
     filter: ['!', ['has', 'point_count']],
     paint: {
       'circle-color': '#11b4da',
@@ -85,7 +85,7 @@ map.on('load', () => {
       layers: ['clusters'],
     });
     const clusterId = features[0].properties.cluster_id;
-    map.getSource('earthquakes').getClusterExpansionZoom(
+    map.getSource('campgrounds').getClusterExpansionZoom(
       clusterId,
       (err, zoom) => {
         if (err) return;
