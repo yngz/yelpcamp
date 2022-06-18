c59580a88045c486caafdae4c13927856e2326a8
Yang Zou <yang0201@outlook.com>
2022-06-18 16:32:34 +0100

    Refactor the ejs vs autoformat hack

--- public/scripts/showPageMap.js
+++ public/scripts/showPageMap.js
@@ -1,20 +1,19 @@
 /* eslint-disable */
 mapboxgl.accessToken = mapboxToken;
-campground_obj = JSON.parse(campground);
 
 const map = new mapboxgl.Map({
   container: 'map', // container ID
   style: 'mapbox://styles/mapbox/streets-v11', // style URL
-  center: campground_obj.geometry.coordinates, // starting position [lng, lat]
+  center: campground.geometry.coordinates, // starting position [lng, lat]
   zoom: 10, // starting zoom
 });
 
 new mapboxgl.Marker()
-  .setLngLat(campground_obj.geometry.coordinates)
+  .setLngLat(campground.geometry.coordinates)
   .setPopup(
     new mapboxgl.Popup({ offset: 25 })
       .setHTML(
-        `<h3>${campground_obj.title}</h3><p>${campground_obj.location}</p>`,
+        `<h3>${campground.title}</h3><p>${campground.location}</p>`,
       ),
   )
   .addTo(map);
